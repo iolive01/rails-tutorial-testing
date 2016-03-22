398b216bcc297927a8bfd2b0b17c865ad07fcc68
iolive01 <iris.oliver@gmail.com>
2016-03-22 19:43:02 +0000

    added testing file, doesn't really work

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+#IN PROGRESS, NEEDS TO BE FIXED
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  test "layout links" do
+    get root
+    assert_te
+  # test "the truth" do
+  #   assert true
+  # end
+end
